7d119e6a05bb2ebd02180c3a8699949a4280c173
yuzo-hikida <0601uzo@gmail.com>
2019-10-01 18:28:56 +0900

    重複しているコードを削除

--- config/app.php
+++ config/app.php
@@ -124,7 +124,6 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
-        Collective\Html\HtmlServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
