2d15b0012fac3b989e1fb2e66775323659f81a0c
yuzo-hikida <0601uzo@gmail.com>
2019-10-02 00:22:53 +0900

    migrateファイルのidカラムの桁数修正

--- database/migrations/2019_09_18_092511_create_daily_reports_table.php
+++ database/migrations/2019_09_18_092511_create_daily_reports_table.php
@@ -14,7 +14,7 @@ class CreateDailyReportsTable extends Migration
     public function up()
     {
         Schema::create('daily_reports', function (Blueprint $table) {
-            $table->increments('id', 1000);
+            $table->increments('id');
             $table->integer('user_id');
             $table->string('title', 255);
             $table->string('content',2000);
